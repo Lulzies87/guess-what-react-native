cfb5f3bb71d26b5755f164331fab5c0c3de0d4db
Lilach E <lilachelayza@gmail.com>
2024-06-17 16:34:49 +0300

    add registerForm component and link to login form

--- components/RegisterForm.tsx
+++ components/RegisterForm.tsx
@@ -3,11 +3,12 @@ import { useForm } from "@tanstack/react-form";
 import { StyleSheet, TextInput, TouchableOpacity, View } from "react-native";
 import { ThemedText } from "./ThemedText";
 
-export default function LoginForm() {
+export default function RegisterForm() {
   const form = useForm({
     defaultValues: {
       username: "",
       password: "",
+      confirmPassword: "",
     },
     onSubmit: async ({ value }) => {
       console.log(value);
@@ -44,10 +45,24 @@ export default function LoginForm() {
             />
           )}
         />
+        <form.Field
+          name="confirmPassword"
+          children={(field) => (
+            <TextInput
+              style={styles.formField}
+              placeholder="Confirm Password"
+              placeholderTextColor={"#888"}
+              secureTextEntry={true}
+              value={field.state.value}
+              onBlur={field.handleBlur}
+              onChangeText={(text) => field.handleChange(text)}
+            />
+          )}
+        />
       </View>
 
       <TouchableOpacity style={styles.button} onPress={form.handleSubmit}>
-        <ThemedText style={styles.buttonText}>Login</ThemedText>
+        <ThemedText style={styles.buttonText}>Register</ThemedText>
       </TouchableOpacity>
     </View>
   );
@@ -57,6 +72,7 @@ const styles = StyleSheet.create({
   formContainer: {
     flex: 1,
     alignItems: "center",
+    width: "100%",
   },
   formFieldsContainer: {
     flex: 1,

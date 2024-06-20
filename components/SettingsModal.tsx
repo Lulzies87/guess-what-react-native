e031bc40f25e47865fb0f11357d0f665a526ff62
Lilach E <lilachelayza@gmail.com>
2024-06-20 16:03:51 +0300

    add settings modal

---
+++ components/SettingsModal.tsx
@@ -0,0 +1,64 @@
+import React, { ReactNode } from "react";
+import { View, StyleSheet, Modal, Pressable } from "react-native";
+import { ThemedText } from "./ThemedText";
+import { MaterialIcons } from "@expo/vector-icons";
+
+type SettingsModalProps = {
+  isVisible: boolean;
+  children: ReactNode;
+  onClose: () => void;
+};
+
+export default function SettingsModal({
+  isVisible,
+  children,
+  onClose,
+}: SettingsModalProps) {
+  return (
+    <Modal animationType="fade" transparent={true} visible={isVisible}>
+      <View style={styles.overlay}>
+        <View style={styles.contnainer}>
+          <View style={styles.titleContainer}>
+            <ThemedText type="subtitle">Settings</ThemedText>
+            <Pressable onPress={onClose}>
+              <MaterialIcons name="close" color="black" size={22} />
+            </Pressable>
+          </View>
+          {children}
+        </View>
+      </View>
+    </Modal>
+  );
+}
+const styles = StyleSheet.create({
+  overlay: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  contnainer: {
+    height: "40%",
+    width: "80%",
+    backgroundColor: "whitesmoke",
+    alignItems: "center",
+    borderRadius: 18,
+    borderColor: "lightgrey",
+    shadowOffset: {
+      width: 3,
+      height: 3,
+    },
+    shadowOpacity: 0.2,
+    shadowColor: "dimgrey",
+  },
+  titleContainer: {
+    width: "100%",
+    height: "15%",
+    backgroundColor: "gainsboro",
+    borderTopRightRadius: 18,
+    borderTopLeftRadius: 18,
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+});

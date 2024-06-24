b4712b6c6ef6ed3c904fa9ccf011324cb11f2eac
Lilach E <lilachelayza@gmail.com>
2024-06-24 11:42:41 +0300

    add camera component and flip camera

---
+++ app/takePicturePage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Camera from "@/components/Camera";
+
+export default function takePicturePage() {
+  return <Camera />;
+}
+                                                                        

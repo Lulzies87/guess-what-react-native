be9caddf27ca99f2df430a33c3338b8584af82b3
Lilach E <lilachelayza@gmail.com>
2024-06-28 09:09:11 +0300

    update players and challenges models

---
+++ api/src/challenges.model.ts
@@ -0,0 +1,29 @@
+import { Schema, model } from "mongoose";
+
+type Word = {
+  word: string;
+  description: {
+    wordType: "verb" | "noun" | "adjective";
+    wordNumber: "singular" | "plural";
+    imageName: string;
+  };
+};
+
+export type Challenge = {
+  storyId: string;
+  challengeCreatorId: string;
+  chosenWords: {
+    firstWord: Word;
+    secondWord: Word;
+    thirdWord: Word;
+    fourthWord: Word;
+  };
+};
+
+export const challengeSchema = new Schema<Challenge>({
+  storyId: { type: String, required: true },
+  challengeCreatorId: { type: String, required: true },
+  chosenWords: { type: Object, required: true },
+});
+
+export const ChallengeModel = model("Challenge", challengeSchema, "challenges");
